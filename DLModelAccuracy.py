41b58637e43e2a8b054f66a302576e2478df2eb4
HuizeMao <38351685+HuizeMao@users.noreply.github.com>
2019-01-15 22:58:10 +0100

    Update and rename TestAccuracy.py to DLModelAccuracy.py

--- DLModelAccuracy.py
+++ DLModelAccuracy.py
@@ -5,12 +5,12 @@ import cv2
 import keras
 from keras.models import Model, load_model
 
-model = load_model('ResNet50.h5')
+model = load_model('DLModel.h5')
 #load test set labeled pairs
 X_test = np.load('dataset/X_test.npy')
 Y_test = np.load('dataset/Y_test.npy')
 
-TestAccuracy = model.evaluate(X_CV, Y_CV)
+TestAccuracy = model.evaluate(X_test, Y_test)
 print ("Loss = " + str(preds[0]))
 print ("Test Accuracy = " + str(preds[1]))
 model.summary
