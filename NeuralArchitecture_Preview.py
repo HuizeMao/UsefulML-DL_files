5f74cce74ddfd8e66bcf3d04d7beda0eeaf2166c
HuizeMao <38351685+HuizeMao@users.noreply.github.com>
2019-01-31 20:04:40 +0100

    Create NeuralArchitecture_Preview.py

---
+++ NeuralArchitecture_Preview.py
@@ -0,0 +1,13 @@
+from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D, Dropout
+from keras.initializers import glorot_uniform
+from keras.models import Model, load_model
+from keras import layers
+
+#create your model
+
+#....
+#....
+
+
+model = model((shape),classes)
+print(model.summary())
